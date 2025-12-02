c6f92a4a6c37b02eebeff81961660900fc2e4909
Sávio Gomes da Silva <saviogomesdasilvadev@gmail.com>
2025-12-02 15:15:38 -0300

    colocando rotas

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/ping', function () {
     return response()->json(['status' => 'Laravel conectado com sucesso!']);
 });
+Route::get('/usuarios', function () {
+    return response()->json(['status' => 'Laravel conectado com sucesso!']);
+});
+Route::get('/administradores', function () {
+    return response()->json(['status' => 'Laravel conectado com sucesso!']);
+});
+Route::get('/cadastros', function () {
+    return response()->json(['status' => 'Laravel conectado com sucesso!']);
+});
+Route::get('/outros', function () {
+    return response()->json(['status' => 'Laravel conectado com sucesso!']);
+});
 
 
 Route::get('/user', function (Request $request) {
